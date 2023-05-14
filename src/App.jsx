47151b78304b789afb6ad5f5979a3f8c3cc74916
Raykkoner Dujhkkovick Silva de Farias <raykkonerd@gmail.com>
2023-05-14 20:20:23 -0300

    Indentation adjust

--- src/App.jsx
+++ src/App.jsx
@@ -10,15 +10,15 @@ export default function App() {
 	const [dataPrazo, setDataPrazo] = useState(dataAtual);
 	
   return <Calendar 
-					onChange={setDataPrazo} 
-					value={dataPrazo} 
-					calendarType='US'
-					showNeighboringMonth={false} 
-					navigationLabel={({ label }) => (label.split(' ')[0].charAt(0).toUpperCase() + label.split(' ')[0].slice(1))}
-					formatShortWeekday={(locale, dd) => ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'][dd.getDay()]}
-					minDate={dataAtual} 
-					maxDate={daquiAUmMes} 
-					minDetail="year"
-					tileDisabled={({date}) => !date.getDay()}
-					/>
+		onChange={setDataPrazo} 
+		value={dataPrazo} 
+		calendarType='US'
+		showNeighboringMonth={false} 
+		navigationLabel={({ label }) => (label.split(' ')[0].charAt(0).toUpperCase() + label.split(' ')[0].slice(1))}
+		formatShortWeekday={(locale, dd) => ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'][dd.getDay()]}
+		minDate={dataAtual} 
+		maxDate={daquiAUmMes} 
+		minDetail="year"
+		tileDisabled={({date}) => !date.getDay()}
+		/>
 }
